6dce5701f343fa01bfb38141360ba43c5a8876bf
Amna Fatima <amnafatimaa6@gmail.com>
2026-04-17 05:49:03 -0700

    Update news.py

--- news.py
+++ news.py
@@ -1,6 +1,7 @@
 import feedparser
 
 def get_news():
+
     feeds = [
         "http://feeds.bbci.co.uk/news/rss.xml",
         "http://rss.cnn.com/rss/edition.rss"
@@ -11,11 +12,11 @@ def get_news():
     for f in feeds:
         feed = feedparser.parse(f)
 
-        for e in feed.entries[:4]:
+        for e in feed.entries[:6]:
             articles.append({
                 "title": e.get("title", ""),
                 "summary": e.get("summary", ""),
                 "link": e.get("link", "#")
             })
 
-    return articles[:8]
+    return articles
